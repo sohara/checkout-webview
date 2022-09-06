d57c3bcf5dfee58149958b1f352edfd6dff7932c
Sean O'Hara <sohara@sohara.com>
2022-09-06 14:31:08 -0400

    Add basic webview to load checkout, with refresh UI

--- CheckoutInWebview/ViewController.swift
+++ CheckoutInWebview/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate {
 
+    var webView: WKWebView!
+    
+    var url = URL(string: "https://merchant.platform-qa.breadgateway.com/host")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let request = URLRequest(url: url!)
+        webView.load(request)
+        setupRefreshControl()
         // Do any additional setup after loading the view.
     }
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        webView.scrollView.bounces = true
+        view = webView
+    }
+    private func setupRefreshControl() {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshWebView(sender:)), for: UIControl.Event.valueChanged)
+        webView.scrollView.addSubview(refreshControl)
+    }
 
+    @objc
+    private func refreshWebView(sender: UIRefreshControl) {
+        print("refreshing...")
+        webView.load(URLRequest(url: url!))
+        sender.endRefreshing()
+    }
 
 }
 
